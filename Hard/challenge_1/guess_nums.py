01a2ff8aea97b9ac638b206666f070bad0b66056
Miles Sorlie <sorliem@kenteches.com>
2015-04-01 16:26:00 -0600

    Finished challenge 1 hard in python!

--- Hard/challenge_1/guess_nums.py
+++ Hard/challenge_1/guess_nums.py
@@ -9,19 +9,26 @@ def guess():
 	while (True):
 		guess = rand.randrange(len(guessRange)) + guessRange[0];
 		print("Guess: " + str(guess))
-		ans = input("(h)igh or (l)ow? ")
+		ans = input("(h)igh or (l)ow or (c)orrect? ")
 		if (ans == "l"):
-			guessRange = chopLower(guess)
+			guessRange = chopLower(guess, guessRange)
 		elif (ans == "h"):
-			guessRange = chopUpper(guess)
+			guessRange = chopUpper(guess, guessRange)
 		else:
 			print("(borg-beep-boop) yay I found it!")
+			break;
 
-def chopUpper(pt):
-	pass #todo
+def chopUpper(pt, rng):
+	"""From the pt in the range given, return rng[0:i]"""
+	for i in range(len(rng)):
+		if rng[i] == pt:
+			return rng[0:i]
 
-def chopLower(pt):
-	pass #todo
+def chopLower(pt, rng):
+	"""From index 0 to the range given, return rng[i+1:len(rng)]"""
+	for i in range(len(rng)):
+		if rng[i] == pt:
+			return rng[i+1:len(rng)]
 
 if __name__ == "__main__":
 	guess()
