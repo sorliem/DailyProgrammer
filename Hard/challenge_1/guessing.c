fea065dec4ecb466af49687a1d1d0e91de5a7ac9
Miles Sorlie <sorliem@kenteches.com>
2015-03-31 22:09:29 -0600

    Reorganized folder structure. Added challenge #1 hard

---
+++ Hard/challenge_1/guessing.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(int argc, char ** argv) {
+	
+	printf("Welcome to the guessing game.\n Think of a number between " +
+		"0 and 100 and I'll try to guess it. Ready?");
+	
+	return 0;
+}
